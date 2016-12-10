f40e088881d50cab9e24180ec66923358596ea7c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2016-12-10 15:33:44 +0900

    OS X 用のプロジェクトを追加

---
+++ SQLiteTest/SQLiteTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SQLiteTest
+//
+//  Created by tsuchiyamamasaru on 2016/12/10.
+//  Copyright © 2016年 tsuchiyamamasaru. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
